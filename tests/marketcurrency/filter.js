6d444f4c43d10753f794100882346cf00f2f6715
charles.aldarondo@gmail.com <moriah9879>
2014-11-08 08:20:20 -0700

    Currency Formatting Commas or Periods

--- tests/marketcurrency/filter.js
+++ tests/marketcurrency/filter.js
@@ -7,11 +7,23 @@ describe('The marketcurrency filter', function() {
     var $window;
     var currencyList = {
         "ARS": [0.5, "AR$", 2, ".", ","],
-        "USD": [1, "$", 2, ",", "."],
-        "GBP": [0.5, "£", 0, ",", "."],
-        "AUD": [0.5, "$", 2, ",", "."],
+        "AMD": [0.5, "AMD", 2, ",", "."],
+        "AWG": [0.5, "AWG", 2, ",", "."],
+        "AUD": [0.5, "AUD$", 2, " ", "."],
+        "BRL": [0.5, "BRL", 2, ".", ","],
+        "CAD": [0.5, "$", 2, ",", "."],
+        "CHF": [0.5, "CHF", 2, "'", "."],
+        "CNY": [0.5, "CNY", 2, ",", "."],
         "EUR": [0.5, "€", 2, ",", "."],
-        "CAD": [0.5, "$", 2, ",", "."]
+        "GBP": [0.5, "£", 2, ",", "."],
+        "HKD": [1, "HKD$", 2, ",", "."],
+        "INR": [0.5, "INR", 2, ",", "."],
+        "JPY": [0.5, "JPY", 0, ",", ""],
+        "MXN": [0.5, "MXN", 2, ",", "."],
+        "NZD": [1, "NZD$", 2, ",", "."],
+        "RUB": [2, "RUB", 2, ".", ","],
+        "SEK": [0.5, "SEK", 2, ".", ","],
+        "USD": [1, "$", 2, ",", "."]
     };
 
     beforeEach(module('market.filters'));
@@ -29,110 +41,242 @@ describe('The marketcurrency filter', function() {
 
     it('should default to USD', function() {
         // Arrange
-        var input = 100;
+        var input = 1000;
 
         // Act
         var result = $filter('marketcurrency')(input, currencyList);
 
         // Assert
-        expect(result).toEqual('$100.00');
+        expect(result).toEqual('$1,000.00');
     });
 
     it('should default to USD for invalid language', function() {
         // Arrange
         $window.navigator.userLanguage = 'qa-qa';
         $window.navigator.language = 'qa-qa';
-        var input = 100;
+        var input = 1000;
 
         // Act
         var result = $filter('marketcurrency')(input, currencyList);
 
         // Assert
-        expect(result).toEqual('$100.00');
+        expect(result).toEqual('$1,000.00');
     });
 
     it('should default to GBP when en-gb', function() {
         // Arrange
         $window.navigator.userLanguage = 'en-gb';
-        var input = 100;
+        var input = 2000;
 
         // Act
         var result = $filter('marketcurrency')(input, currencyList);
 
         // Assert
-        expect(result).toEqual('£50');
-    });
-
-    it('should read USD cookie', function() {
-        // Arrange
-        $cookies.preferences = 'currency=USD';
-        var input = 100;
-
-        // Act
-        var result = $filter('marketcurrency')(input, currencyList);
-
-        // Assert
-        expect(result).toEqual('$100.00');
-    });
-
-    it('should read GBP cookie', function() {
-        // Arrange
-        $cookies.preferences = 'currency=GBP';
-        var input = 100;
-
-        // Act
-        var result = $filter('marketcurrency')(input, currencyList);
-
-        // Assert
-        expect(result).toEqual('£50');
-    });
-
-    it('should read AUD cookie', function() {
-        // Arrange
-        $cookies.preferences = 'currency=AUD';
-        var input = 100;
-
-        // Act
-        var result = $filter('marketcurrency')(input, currencyList);
-
-        // Assert
-        expect(result).toEqual('$50.00');
-    });
-
-    it('should read EUR cookie', function() {
-        // Arrange
-        $cookies.preferences = 'currency=EUR';
-        var input = 100;
-
-        // Act
-        var result = $filter('marketcurrency')(input, currencyList);
-
-        // Assert
-        expect(result).toEqual('€50.00');
-    });
-
-    it('should read CAD cookie', function() {
-        // Arrange
-        $cookies.preferences = 'currency=CAD';
-        var input = 100;
-
-        // Act
-        var result = $filter('marketcurrency')(input, currencyList);
-
-        // Assert
-        expect(result).toEqual('$50.00');
+        expect(result).toEqual('£1,000.00');
     });
 
     it('should read ARS cookie', function() {
         // Arrange
         $cookies.preferences = 'currency=ARS';
-        var input = 100;
+        var input = 2000;
 
         // Act
         var result = $filter('marketcurrency')(input, currencyList);
 
         // Assert
-        expect(result).toEqual('AR$50,00');
+        expect(result).toEqual('AR$1.000,00');
+    });
+
+    it('should read AMD cookie', function() {
+        // Arrange
+        $cookies.preferences = 'currency=AMD';
+        var input = 2000;
+
+        // Act
+        var result = $filter('marketcurrency')(input, currencyList);
+
+        // Assert
+        expect(result).toEqual('AMD1,000.00');
+    });
+
+    it('should read AWG cookie', function() {
+        // Arrange
+        $cookies.preferences = 'currency=AWG';
+        var input = 2000;
+
+        // Act
+        var result = $filter('marketcurrency')(input, currencyList);
+
+        // Assert
+        expect(result).toEqual('AWG1,000.00');
+    });
+
+    it('should read AUD cookie', function() {
+        // Arrange
+        $cookies.preferences = 'currency=AUD';
+        var input = 2000;
+
+        // Act
+        var result = $filter('marketcurrency')(input, currencyList);
+
+        // Assert
+        expect(result).toEqual('AUD$1 000.00');
+    });
+
+    it('should read CAD cookie', function() {
+        // Arrange
+        $cookies.preferences = 'currency=CAD';
+        var input = 2000;
+
+        // Act
+        var result = $filter('marketcurrency')(input, currencyList);
+
+        // Assert
+        expect(result).toEqual('$1,000.00');
+    });
+
+    it('should read CHF cookie', function() {
+        // Arrange
+        $cookies.preferences = 'currency=CHF';
+        var input = 2000;
+
+        // Act
+        var result = $filter('marketcurrency')(input, currencyList);
+
+        // Assert
+        expect(result).toEqual("CHF1'000.00");
+    });
+
+    it('should read CNY cookie', function() {
+        // Arrange
+        $cookies.preferences = 'currency=CNY';
+        var input = 2000;
+
+        // Act
+        var result = $filter('marketcurrency')(input, currencyList);
+
+        // Assert
+        expect(result).toEqual('CNY1,000.00');
+    });
+
+    it('should read EUR cookie', function() {
+        // Arrange
+        $cookies.preferences = 'currency=EUR';
+        var input = 2000;
+
+        // Act
+        var result = $filter('marketcurrency')(input, currencyList);
+
+        // Assert
+        expect(result).toEqual('€1,000.00');
+    });
+
+    it('should read GBP cookie', function() {
+        // Arrange
+        $cookies.preferences = 'currency=GBP';
+        var input = 2000;
+
+        // Act
+        var result = $filter('marketcurrency')(input, currencyList);
+
+        // Assert
+        expect(result).toEqual('£1,000.00');
+    });
+
+    it('should read HKD cookie', function() {
+        // Arrange
+        $cookies.preferences = 'currency=HKD';
+        var input = 1000;
+
+        // Act
+        var result = $filter('marketcurrency')(input, currencyList);
+
+        // Assert
+        expect(result).toEqual('HKD$1,000.00');
+    });
+
+    it('should read INR cookie', function() {
+        // Arrange
+        $cookies.preferences = 'currency=INR';
+        var input = 2000;
+
+        // Act
+        var result = $filter('marketcurrency')(input, currencyList);
+
+        // Assert
+        expect(result).toEqual('INR1,000.00');
+    });
+
+    it('should read JPY cookie', function() {
+        // Arrange
+        $cookies.preferences = 'currency=JPY';
+        var input = 2000;
+
+        // Act
+        var result = $filter('marketcurrency')(input, currencyList);
+
+        // Assert
+        expect(result).toEqual('JPY1,000');
+    });
+
+    it('should read MXN cookie', function() {
+        // Arrange
+        $cookies.preferences = 'currency=MXN';
+        var input = 2000;
+
+        // Act
+        var result = $filter('marketcurrency')(input, currencyList);
+
+        // Assert
+        expect(result).toEqual('MXN1,000.00');
+    });
+
+    it('should read NZD cookie', function() {
+        // Arrange
+        $cookies.preferences = 'currency=NZD';
+        var input = 1000;
+
+        // Act
+        var result = $filter('marketcurrency')(input, currencyList);
+
+        // Assert
+        expect(result).toEqual('NZD$1,000.00');
+    });
+
+    it('should read RUB cookie', function() {
+        // Arrange
+        $cookies.preferences = 'currency=RUB';
+        var input = 1000;
+
+        // Act
+        var result = $filter('marketcurrency')(input, currencyList);
+
+        // Assert
+        expect(result).toEqual('RUB2.000,00');
+    });
+
+    it('should read SEK cookie', function() {
+        // Arrange
+        $cookies.preferences = 'currency=SEK';
+        var input = 2000;
+
+        // Act
+        var result = $filter('marketcurrency')(input, currencyList);
+
+        // Assert
+        expect(result).toEqual('SEK1.000,00');
+    });
+
+    it('should read USD cookie', function() {
+        // Arrange
+        $cookies.preferences = 'currency=USD';
+        var input = 1000;
+
+        // Act
+        var result = $filter('marketcurrency')(input, currencyList);
+
+        // Assert
+        expect(result).toEqual('$1,000.00');
     });
 
     it('should default to USD with bad cookie', function() {
@@ -192,7 +336,7 @@ describe('The marketcurrency filter', function() {
         var result = $filter('marketcurrency')(input, currencyList);
 
         // Assert
-        expect(result).toEqual('£50');
+        expect(result).toEqual('£50.00');
         expect($cookies.preferences).toEqual('currency=GBP');
     });
 });
